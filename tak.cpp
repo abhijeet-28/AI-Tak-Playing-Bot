af8c45eae60542f334962f7884e9ccbbaba7f5af
Kapil Kumar <masterkapilkumar@gmail.com>
2016-10-02 04:53:11 +0530

    Partially generated valid moves. Abhijeet- generate_stack_moves waala function likh dena

--- tak.cpp
+++ tak.cpp
@@ -165,6 +165,103 @@ void execute_move(string move_string) {
 	turn = 1 - turn;
 }
 
+// vector<vector<int>> partition(n){
+	// vector<vector<int>> part_list;
+	// part_list.push_back(vector<int>(1,n));
+	// for(int i=1; i<n; i++) {
+		// for(auto const& y: partition(n - i) {
+			// vector<int> temp;
+			// temp.push_back(i);
+			// temp.insert( temp.end(), y.begin(), y.end() );
+			// part_list.push_back(temp);
+		// }
+	// }
+	// return part_list;
+// }
+
+// def check_valid(self, square, direction, partition):
+	// '''For given movement (partition), check if stack on
+	// square can be moved in direction. Assumes active player
+	// is topmost color
+	// '''
+	// if direction == '+':
+		// change = self.n
+	// elif direction == '-':
+		// change = -self.n
+	// elif direction == '>':
+		// change = 1
+	// elif direction == '<':
+		// change = -1
+	// for i in xrange(len(partition)):
+		// next_square = square + change * (i + 1)
+		// if len(self.board[next_square]) > 0 and self.board[next_square][-1][1] == 'C':
+			// return False
+		// if len(self.board[next_square]) > 0 and self.board[next_square][-1][1] == 'S' and i != len(partition) - 1:
+			// return False
+		// if i == len(partition) - 1 and len(self.board[next_square]) > 0 and self.board[next_square][-1][1] == 'S' and partition[i] > 1:
+			// return False
+		// if i == len(partition) - 1 and len(self.board[next_square]) > 0 and self.board[next_square][-1][1] == 'S' and self.board[square][-1][1] != 'C':
+			// return False
+	// return True
+
+// vector<string> generate_stack_moves(vector<string> all_moves, int row, int col) {
+	// int size = board[row][col].size();
+	// char dirs[] = {'+', '-', '<', '>'};
+	// int up = n - row;
+	// int down = row - 1;
+	// int right = n - col;
+	// int left = col - 1;
+	// int rem_squares = {up, down, left, right};
+	// size = min(size, n);
+	// for(int i=0; i<size; i++) {
+		// vector<vector<int>> part_list = partition(i + 1);
+		// for(int j=0; j<4; j++) {
+			// part_dir = [part for part in part_list if len(part) <= rem_squares[di]]
+			// for part in part_dir:
+				// if self.check_valid(square, dirs[di], part):
+					// part_string = ''.join([str(i) for i in part])
+					// all_moves.append(str(sum(part)) + self.all_squares[square] + dirs[di] + part_string)
+		// }
+	// }
+	// return all_moves
+	
+// }
+
+vector<string> generate_all_moves(int player) {
+	
+	vector<string> all_moves;
+	for(int i=1; i<=n; i++) {
+		for(int j=1; j<=n; j++) {
+			if(board[i][j].empty()) {
+				if(players[player].flats > 0) {
+					string move="F";
+					move+=(char)(j+96);
+					move+=to_string(n-i+1);
+					all_moves.push_back(move);
+				}
+				if(moves != player && players[player].flats > 0) {
+					string move="S";
+					move+=(char)(j+48);
+					move+=to_string(n-i+1);
+					all_moves.push_back(move);
+				}
+				if(moves != player && players[player].caps > 0) {
+					string move="C";
+					move+=(char)(j+48);
+					move+=to_string(n-i+1);
+					all_moves.push_back(move);
+				}
+			}
+		}
+	}
+	for(int i=1; i<=n; i++) {
+		for(int j=1; j<=n; j++) {
+			if((!board[i][j].empty()) && (board[i][j].top().first == player) && (moves != player))
+				all_moves += self.generate_stack_moves(all_moves, i, j);
+	}
+	return all_moves;
+}
+
 void play() {
 	string move;
 	
@@ -174,10 +271,17 @@ void play() {
 	}
 	
 	while(true) {
+		
+		vector<string> all_moves = generate_all_moves(player_num);
+		cout<<"\n";
+		for(int i=0;i<all_moves.size();i++) {
+			cout<<all_moves[i]<<"   ";
+		}
+		cout<<"\n\n";
 		//print the board
 		vector<vector<stack<pair<int,int>>>> temp(board);
-		for(int i=1; i<temp.size(); i++) {
-			for(int j=1; j<temp[0].size(); j++) {
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=n; j++) {
 				if(temp[i][j].empty())
 					cout<<"-";
 				while(!temp[i][j].empty()) {
