bf8d738288ea56fae5fcd119c4c3751c63add73e
Kapil Kumar <masterkapilkumar@gmail.com>
2016-10-01 19:44:24 +0530

    Parsed moves

--- tak.cpp
+++ tak.cpp
@@ -8,78 +8,118 @@
 using namespace std;
 
 void execute_move(string );
-
+void play();
 
 struct Player {
 	int flats;
 	int caps;
-};
+}players[2];
 
-struct Game {
-	int total_squares;
-	bool turn;
-	int max_flats;
-	int max_capstones;
-};
+bool turn = 0;
+int max_flats;
+int max_capstones;
 
-vector<vector<int>> board;
+vector<vector<pair<int,string>>> board;
 int n;
+int moves=0;
+int player_num, time_limit;
 
 
 int main() {
 	
 	std::ios::sync_with_stdio(false);
 	
-	int player_num, time;
-	string move;
+	cin>>player_num>>n>>time_limit;
+	cin.ignore();
+	player_num-=1;
 	
-	cin>>player_num>>n>>time;
-	
-	Game game = {n*n,0,0,0};
-	
-	board.resize(n);
-	for(int i=0; i<n; i++) {
-		board[i].resize(n);
+	board.resize(n+1);
+	for(int i=0; i<n+1; i++) {
+		board[i].resize(n+1);
 	}
 	
 	if(n == 5) {
-		game.max_flats = 21;
-		game.max_capstones = 1;
+		max_flats = 21;
+		max_capstones = 1;
 	}
 	else if(n == 6){
-		game.max_flats = 30;
-		game.max_capstones = 1;
+		max_flats = 30;
+		max_capstones = 1;
 	}
 	else if(n == 7){
-		game.max_flats = 40;
-		game.max_capstones = 1;
+		max_flats = 40;
+		max_capstones = 1;
 	}
 	
-	if(player_num == 1) {
-		getline(cin,move);
-		getline(cin,move);
-		execute_move(move);
-	}
-}
-
-int square_to_num(string square_str) {
-	int row = (int)square_str[0] - 96;
-	int col = square_str[1]-'0';
-	if(row < 1 or row > n or col < 1 or col > n)
-		return -1;
-	return n * (col - 1) + (row - 1)
+	players[1].flats = max_flats;
+	players[2].flats = max_flats;
+	players[1].caps = max_capstones;
+	players[2].caps = max_capstones;
+	
+	play();
 }
 
 void execute_move(string move_string) {
 	
+	int row, col, count, change, next_count;
+	bool current_piece;
+	
+	if(turn == 0)
+		moves += 1;
+	if(moves != 1)
+		current_piece = turn;
+	else
+		current_piece = 1 - turn;
+	
 	if(isalpha(move_string[0])) {
-		int square = square_to_num(move_string.substr(1));
-		cout<<square;
-		// if move_string[0] == 'F' or move_string[0] == 'S':
-			// self.board[square].append((current_piece, move_string[0]))
-			// self.players[current_piece].flats -= 1
-		// elif move_string[0] == 'C':
-			// self.board[square].append((current_piece, move_string[0]))
-			// self.players[current_piece].capstones -= 1
+		row = n - move_string[2]+'0' + 1;
+		col = (int)move_string[1] - 96;
+		
+		// cout<<row<<"   "<<col;
+		if(move_string[0] == 'F' or move_string[0] == 'S') {
+			board[row][col] = make_pair(current_piece, move_string[0]);
+			players[current_piece].flats -= 1;
+		}
+		else if(move_string[0] == 'C') {
+			board[row][col] = make_pair(current_piece, 'C');
+			players[current_piece].caps -= 1;
+		}
+	}
+	else if(isdigit(move_string[0])) {
+		count = move_string[0]-'0';
+		row = n - move_string[2]+'0' + 1;
+		col = (int)move_string[1] - 96;
+		char direction = move_string[3];
+		int prev_row = row;
+		int prev_col = col;
+		if(direction == '+') {
+			for(int i=4; i<move_string.length(); i++) {
+				next_count = move_string[i]-'0';
+				int next_row = prev_row - next_count;
+				
+			}
+		}
+		else if(direction == '-')
+			change = -n;
+		else if(direction == '>')
+			change = 1;
+		elif(direction == '<')
+			change = -1;
+		
+		count = int(move_string[0]);
+		// self.board[square] = self.board[square][:-count]
+	}
+	turn = 1 - turn;
+}
+
+void play() {
+	string move;
+	
+	if(player_num == 1) {
+		getline(cin,move);
+		execute_move(move);
+	}
+	while(1) {
+		cout<<
 	}
 }
